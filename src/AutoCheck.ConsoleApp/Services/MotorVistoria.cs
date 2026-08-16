3f5aa0791d272b29dea6ddb25d7e0d6877209ff4
Vlade <vladecassia@gmail.com>
2026-08-16 19:43:39 -0300

    Adiciona motor de pontuação da vistoria

--- src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
+++ src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
@@ -1,7 +1,75 @@
+using AutoCheck.ConsoleApp.Models;
+
 namespace AutoCheck.ConsoleApp.Services
 {
     public class MotorVistoria
     {
-        
+        public int CalculadoraPontuacaoItem(ItemVistoria item)
+        {
+            if (item.Status == "Bom")
+            {
+                return 10;
+            }
+            else if (item.Status == "Regular")
+            {
+                return 5;
+            }
+            else if (item.Status == "Ruim")
+            {
+                return 0;
+            }
+            return 0;
+        }
+
+        public int CalcularPontuacaoTotal(Veiculo veiculo)
+        {
+            int pontuacaoTotal = 0;
+
+            foreach (ItemVistoria item in veiculo.VistoriaRealizada)
+            {
+                pontuacaoTotal += CalculadoraPontuacaoItem(item);
+            }
+            return pontuacaoTotal;
+        }
+
+        public int CalcularPontuacaoMaxima(Veiculo veiculo)
+        {
+            int quantidade = 0;
+
+            foreach (ItemVistoria item in veiculo.VistoriaRealizada)
+            {
+                quantidade++;
+            }
+            return quantidade * 10;
+        }
+        public double CalcularPercentual(Veiculo veiculo)
+        {
+            int pontuacaoObtida = CalcularPontuacaoTotal(veiculo);
+            int pontuacaoMaxima = CalcularPontuacaoMaxima(veiculo);
+
+            if (pontuacaoMaxima == 0)
+            {
+                return 0;
+            }
+
+            double percentual = ((double)pontuacaoObtida / pontuacaoMaxima) * 100;
+
+            return percentual;
+        }
+        public string ClassificarVeiculo(double percentual)
+        {
+            if (percentual >= 90)
+            {
+                return "Aprovado com Excelência";
+            }
+            else if (percentual >= 60)
+            {
+                return "Aprovado com Apontamento";
+            }
+            else
+            {
+                return "Reprovado na Vistoria";
+            }
+        }
     }
 }
